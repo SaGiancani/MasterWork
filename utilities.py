3b94b2ac578607087892e93d573dd6a07a3db92e
Salvatore Giancani <salvatore.giancani@c703i-gpu6.uibk.ac.at>
2020-05-17 01:29:45 +0200

    check_cuda added

--- utilities.py
+++ utilities.py
@@ -1,3 +1,5 @@
+import torch
+
 def save_dict_csv(hyperparam_dict):
     w = csv.writer(open(hyperparam_dict['name']+"_hyperParam.csv", "w"))
     for key, val in hyperparam_dict.items():
@@ -7,3 +9,9 @@ def save_dict_txt(hyperparam_dict):
     f = open(hyperparam_dict['name']+'/'+hyperparam_dict['name']+"_hyperParam.txt","w")
     f.write(str(hyperparam_dict) )
     f.close()
+    
+def check_cuda():
+    cuda_available = torch.cuda.is_available()
+    device = torch.device('cuda' if cuda_available else 'cpu')
+    print('cuda_available: {}, device: {}'.format(cuda_available, device))
+    return cuda_available, device
