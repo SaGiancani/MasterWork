74d01ed41843bdf7b317f7b543c9517113c968fe
Salvatore Giancani <salvatore.giancani@c703i-gpu6.uibk.ac.at>
2020-05-26 15:22:26 +0200

    Initialization methods moved on utilities

--- hybrid_cae.py
+++ hybrid_cae.py
@@ -160,30 +160,3 @@ class HybridConvolutionalAutoencoder(nn.Module):
         # Save the model state
         distutils.dir_util.mkpath(filename)
         torch.save(self.state_dict(), filename+'/'+filename +'_model.pt')
-        
-        
-#Understanding the difficulty of training deep feedforward neural networks - Glorot, Xavier & Bengio, Y. (2010)
-def xavier_init_uniform(m):
-    classname = m.__class__.__name__
-    if (type(m) == nn.Linear) or (type(m) == nn.Conv2d):
-        nn.init.xavier_uniform_(m.weight)
-        m.bias.data.zero_()
-
-def xavier_init_normal_(m):
-    classname = m.__class__.__name__
-    if (type(m) == nn.Linear) or (type(m) == nn.Conv2d):
-        nn.init.xavier_normal_(m.weight)
-        m.bias.data.zero_()
-
-#Delving deep into rectifiers: Surpassing human-level performance on ImageNet classification - He, K. et al. (2015)        
-def kaiming_init_normal_(m):
-    classname = m.__class__.__name__
-    if (type(m) == nn.Linear) or (type(m) == nn.Conv2d):
-        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
-        m.bias.data.zero_()
-
-def kaiming_init_uniform(m):
-    classname = m.__class__.__name__
-    if (type(m) == nn.Linear) or (type(m) == nn.Conv2d):
-        nn.init.kaiming_uniform_(m.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
-        m.bias.data.zero_()
